79731d5a68a969a803fd1b72506d91c58d38ea05
Lukas Barbosa <lukas.barbosa@dellead.com>
2022-06-15 17:58:09 -0300

    creating nav-side-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
+import { NavSideBarComponent } from './nav-side-bar/nav-side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavSideBarComponent
   ],
   imports: [
     BrowserModule,
